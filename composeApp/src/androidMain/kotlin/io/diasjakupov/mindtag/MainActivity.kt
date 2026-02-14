c243025a1c9d7d15b4abd54d86029cb096cd0eb3
diasjakupov <dias.jakupov@zimran.io>
2026-02-14 12:42:30 +0500

    chore: remove broken AppAndroidPreview from MainActivity
    Replaced by proper per-screen previews in androidMain feature packages.

--- composeApp/src/androidMain/kotlin/io/diasjakupov/mindtag/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/io/diasjakupov/mindtag/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import io.diasjakupov.mindtag.core.database.DatabaseDriverFactory
 import io.diasjakupov.mindtag.core.di.initKoin
 import io.diasjakupov.mindtag.core.network.TokenStorage
@@ -25,10 +23,4 @@ class MainActivity : ComponentActivity() {
             App()
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
