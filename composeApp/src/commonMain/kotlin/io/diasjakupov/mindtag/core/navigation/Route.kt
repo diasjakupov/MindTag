89f197beee4a4a283b1ebad1b9b78514f187ad35
diasjakupov <dias.jakupov@zimran.io>
2026-02-14 11:27:58 +0500

    refactor: update routes - remove deleted features, rename Practice to Study

--- composeApp/src/commonMain/kotlin/io/diasjakupov/mindtag/core/navigation/Route.kt
+++ composeApp/src/commonMain/kotlin/io/diasjakupov/mindtag/core/navigation/Route.kt
@@ -5,15 +5,11 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 sealed interface Route : NavKey {
-    @Serializable data object Home : Route
     @Serializable data object Library : Route
-    @Serializable data object Practice : Route
-    @Serializable data object Planner : Route
-    @Serializable data object Profile : Route
+    @Serializable data object Study : Route
     @Serializable data class NoteCreate(val noteId: Long? = null) : Route
     @Serializable data class NoteDetail(val noteId: Long) : Route
     @Serializable data class Quiz(val sessionId: String) : Route
     @Serializable data class QuizResults(val sessionId: String) : Route
-    @Serializable data object Onboarding : Route
     @Serializable data object Auth : Route
 }
