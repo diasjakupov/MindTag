92d94d63d1a0f5c1c4d25f045d7d0a486080f4cb
diasjakupov <dias.jakupov@zimran.io>
2026-02-14 11:22:50 +0500

    refactor: delete home, planner, profile, onboarding features for MVP

--- composeApp/src/commonMain/kotlin/io/diasjakupov/mindtag/feature/home/domain/repository/DashboardRepository.kt
+++
@@ -1,8 +0,0 @@
-package io.diasjakupov.mindtag.feature.home.domain.repository
-
-import io.diasjakupov.mindtag.feature.home.domain.model.DashboardData
-import kotlinx.coroutines.flow.Flow
-
-interface DashboardRepository {
-    fun getDashboardData(): Flow<DashboardData>
-}
